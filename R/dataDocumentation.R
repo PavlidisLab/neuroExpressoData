907d3bca6ad27370098b5f4821bc9769243eaf2e
OganM <ogan.mancarcii@gmail.com>
2018-10-01 17:27:43 -0700

    data push

---
+++ R/dataDocumentation.R
@@ -0,0 +1,15 @@
+
+#' Micoarray expression data used in NeuroExpresso.
+'n_expressoExpr'
+
+
+#' Metadata for microarray expression data used in NeuroExpresso
+'n_expressoSamples'
+
+
+
+#' Expression data from Tasic et al. 2016 single cell data
+'TasicMouseExp'
+
+#' Metadata for Tasic et al. 2016 single cell data
+'TasicMouseMeta'
